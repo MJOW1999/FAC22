367d66c5d9838c07323b4d9b0e43f14485c38a2d
michael.jow.williams@gmail.com <michael.jow.williams@gmail.com>
2021-07-04 16:31:32 +0100

    Number 2: Map fullname

--- Week2/JS30/Array-Cardio-1/script.js
+++ Week2/JS30/Array-Cardio-1/script.js
@@ -33,7 +33,9 @@
       console.table(fifteen);
       // Array.prototype.map()
       // 2. Give us an array of the inventors first and last names
-  
+
+    const names = inventors.map((e) => e.first + " " + e.last);
+    console.table(names);
       // Array.prototype.sort()
       // 3. Sort the inventors by birthdate, oldest to youngest
   
