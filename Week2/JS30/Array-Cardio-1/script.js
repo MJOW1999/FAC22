a6af9cf6338f41a22af736eca3a525e708fc5a0a
michael.jow.williams@gmail.com <michael.jow.williams@gmail.com>
2021-07-04 16:49:19 +0100

    Number 4: Total ages

--- Week2/JS30/Array-Cardio-1/script.js
+++ Week2/JS30/Array-Cardio-1/script.js
@@ -38,18 +38,17 @@
     console.table(names);
       // Array.prototype.sort()
       // 3. Sort the inventors by birthdate, oldest to youngest
-      
+
       const oldToYoung = inventors.sort((a,b) => a.year > b.year ? 1:-1);
       console.table(oldToYoung);
   
       // Array.prototype.reduce()
       // 4. How many years did all the inventors live all together?
 
-    //   const totalYears = inventors.reduce((total, inventor) => {
-    //     return total + (inventor.passed - inventor.year);
-    //   }, 0)
-    //   // const oldToYoung = ages.sort((a,b) => a - b);
-    //   console.table(totalYears);
+      const totalYears = inventors.reduce((total, inventor) => {
+        return total + (inventor.passed - inventor.year);
+      }, 0)
+      console.table(totalYears);
   
       // 5. Sort the inventors by years lived
       
