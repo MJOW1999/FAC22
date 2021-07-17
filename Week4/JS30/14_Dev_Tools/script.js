59c01d698a6f41de23d632819e6b68d79e9f3348
MJOW1999 <michael.jow.williams@gmail.com>
2021-07-17 21:28:23 +0100

    Completed Tools

--- Week4/JS30/14_Dev_Tools/script.js
+++ Week4/JS30/14_Dev_Tools/script.js
@@ -7,25 +7,48 @@ function makeGreen() {
 }
 
 // Regular
-
+//console.log("Hello")
 // Interpolated
+//console.log("Hello I %s Michael", "am");
 
 // Styled
+console.log("%c Hello I am Michael", "background: black; color: white; font-size: 4rem")
 
 // warning!
+console.warn("I've got a bad feeling about this");
 
 // Error :|
+console.error("Bad move, Little Ant")
+
 
 // Info
+console.info("FYI: Dragon Ball is my favourite anime")
 
 // Testing
+const p = document.querySelector('p')
+console.assert(p.classList.contains("ouch"), "No, not that!")
 
 // clearing
+//console.clear();
 
 // Viewing DOM Elements
+console.dir(p);
 
 // Grouping together
+dogs.forEach(dog => {
+    console.groupCollapsed(`${dog.name.toUpperCase()}`);
+    console.log(`This is ${dog.name}, they are ${dog.age * 7} dog years old`);
+    console.groupEnd(`${dog.name.toUpperCase()}`)
+})
 
 // counting
+console.count('Michael');
 
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/MJOW1999')
+    .then(data => data.json())
+    .then(data => {
+        console.timeEnd('fetching data');
+        console.log(data);
+    });
