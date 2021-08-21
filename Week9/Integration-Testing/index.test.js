177f73535418550e2785b323d81289cba891999d
MJOW1999 <michael.jow.williams@gmail.com>
2021-08-21 21:55:03 +0100

    Challenge 2 completed

--- Week9/Integration-Testing/index.test.js
+++ Week9/Integration-Testing/index.test.js
@@ -10,5 +10,24 @@ test("Calculate test should handle string inputs", () => {
 // Challenge 2 - Testing UI logic
 
 test("Form should work correctly", () => {
-    
+  // get all three inputs
+  const a = document.querySelector("input[name='a']");
+  const sign = document.querySelector("select");
+  const b = document.querySelector("input[name='b']");
+
+  // enter test data into inputs
+  a.value = "29";
+  sign.value = "*";
+  b.value = "4";
+
+  // submit the form
+  const submitButton = document.querySelector("button[type='submit']");
+  submitButton.click();
+
+  // verify that the page contains the expected result
+  const result = document.querySelector("#result");
+  equal(result.textContent, "116");
+
+  // reset the page so it doesn't affect anything else
+  result.textContent = "";
 })
