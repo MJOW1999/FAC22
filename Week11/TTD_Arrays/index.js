49e94bf7042a8e08d0d096de22907914e44893a6
MJOW1999 <michael.jow.williams@gmail.com>
2021-09-01 18:26:11 +0100

    Implement map testing

--- Week11/TTD_Arrays/index.js
+++ Week11/TTD_Arrays/index.js
@@ -1,3 +1,13 @@
-function map(array) {
-    return array;
+function map(array, fn) {
+    if(!Array.isArray(array)) return "Please pass an array";
+    if(typeof fn !== "function") return "Please pass a mapping function"
+    const result = [];
+
+    for(let i = 0; i < array.length; i++){
+        const item  = array[i];
+        const mappedItem = fn(item);
+        result.push(mappedItem)
+    }
+
+    return result;
 }
--- Week11/TTD_Arrays/index.test.js
+++ Week11/TTD_Arrays/index.test.js
@@ -9,3 +9,16 @@ test("map() should return an array with the same number of elements", () => {
     const result = map([1], (x) => x + 1);
     equal(result[0], 2);
   });
+
+  test("map requires array and fn arguments", () => {
+    equal(map(), "Please pass an array")
+    equal(map([]), "Please pass a mapping function")
+  })
+  test("map should transform 2 elements using fn", () => {
+    const result = map([1, 2], (x) => x + 1);
+    equal(result[1], 3);
+  })
+  test("map should transform 10 elements using fn", () => {
+    const result = map([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (x) => x + 1);
+    equal(result[9], 11);
+  })
